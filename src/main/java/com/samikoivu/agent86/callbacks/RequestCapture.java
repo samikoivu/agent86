2ea6f2fe8ddf758aecdc60422163e4a12d3e1268
Sami Koivu <sami.koivu@gmail.com>
2018-10-30 16:24:01 -0300

    Update src/main/java/com/samikoivu/agent86/callbacks/RequestCapture.java
    Added comments

--- src/main/java/com/samikoivu/agent86/callbacks/RequestCapture.java
+++ src/main/java/com/samikoivu/agent86/callbacks/RequestCapture.java
@@ -47,6 +47,10 @@ public class RequestCapture {
 		this.id = id;
 	}
 
+	/**
+	 * Could make this synchronized - slower, but would guarantee that two requests won't get printed on top of each other
+	 * @param out
+	 */
 	public void printCapturedInfo(PrintStream out) {
 		out.println("===================================================");
 		out.println("Request URI: " + this.uri);
@@ -59,18 +63,34 @@ public class RequestCapture {
 		out.println();
 	}
 
+	/**
+	 * Set request end time in milliseconds
+	 * @param end
+	 */
 	public void setEnd(long end) {
 		this.endMillis = end;
 	}
 
+	/**
+	 * Set the list of unique Strings instantiated during this request
+	 * @param collectedStrings
+	 */
 	public void setStrings(Set<String> collectedStrings) {
 		this.collectedStrings = collectedStrings;
 	}
 
+	/**
+	 * Set the list of classes loaded during this request
+	 * @param classes
+	 */
 	public void setClasses(List<String> classes) {
 		this.classes = classes;
 	}
 
+	/**
+	 * Set the URI this request is referring to
+	 * @param uri
+	 */
 	public void setURI(String uri) {
 		this.uri = uri;
 	}
